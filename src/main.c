62c8dff6295e144c0fecc6c7dbf67f524c6f3ea1
nyanklc <noyanerdinkilic@gmail.com>
2023-10-21 00:39:48 +0300

    introduce globals, o3 optimization

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
+#include "globals.h"
+
 #include <stdio.h>
+
+void system_init()
+{
+  SDL_Init(COMPUT_SDL_INIT_LIST);
+}
+
+void system_quit()
+{
+  SDL_Quit();
+}
 
 int main(int argc, char **argv)
 {
+  system_init();
+
   printf("hhhhssssssssssssssssssss\n");
+
+  system_quit();
   return 0;
 }
