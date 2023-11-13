5c22ecbb765face5be0755dc0e4edd2e99dcd227
nyanklc <noyanerdinkilic@gmail.com>
2023-11-13 19:14:38 +0300

    visual studio script rmdir

--- visual-studio.bat
+++ visual-studio.bat
@@ -1,7 +1,7 @@
 @echo off
 
-DEL /S "build/"
-mkdir "build/"
-cd "build/"
-cmake "-G" "Visual Studio 17 2022" "../"
-cd ".."
+DEL /s /f / "build"
+mkdir build
+cd build
+cmake -G "Visual Studio 17 2022" ../
+cd ../
